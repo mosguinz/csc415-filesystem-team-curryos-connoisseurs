5835a4ced9b345d87f269b439599e44c050a3817
student <arjunsgill132@gmail.com>
2024-04-03 21:17:36 -0700

    fixed free space malloc

--- freespace.c
+++ freespace.c
@@ -18,8 +18,7 @@ int initFreespace(uint64_t numberOfBlocks, uint64_t blockSize){
     // the number of blocks the freespace map needs
     int bytesNeeded = ( numberOfBlocks + BYTESIZE - 1) / BYTESIZE;
     int blocksNeeded = ( bytesNeeded + blockSize - 1) / blockSize;
-    int* freeSpaceList = malloc( sizeof(int) * numberOfBlocks );
-    // mark VCB and freespace map as used
+    int* freeSpaceList = malloc( blocksNeeded * blockSize );
     for( int i = 0; i < numberOfBlocks; i ++ ) {
         freeSpaceList[i] = i+1;
     }
@@ -32,5 +31,14 @@ int initFreespace(uint64_t numberOfBlocks, uint64_t blockSize){
     freeSpaceList[numberOfBlocks - 1] = 0xFFFFFFFF;
 
     int blocksWritten = LBAwrite(freeSpaceList, blocksNeeded, 1);
-    return blocksWritten == -1 ? -1: 1;
+    return blocksWritten == -1 ? -1: blocksNeeded + 1;
 }
+
+int getFreeBlocks(uint64_t numberOfBlocks) {
+    int* freeBlocks;
+	struct VCB * volumeControlBlock;
+    if( LBAread(volumeControlBlock,1, 0 ) == -1) {
+        return -1;
+    }
+}
+
