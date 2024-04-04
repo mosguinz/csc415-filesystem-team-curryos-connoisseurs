498c8cfa139bde640e89eb7bdfd619f10ec62297
student <realvignesh@gmail.com>
2024-04-03 19:55:12 -0700

    Moved structures and create directory to separate interface

---
+++ fs_control.h
@@ -0,0 +1,37 @@
+#ifndef _FSCTL
+#define _FSCTL
+// Specifications for volume control block
+struct VCB
+{
+	long signature; 	// VCB identifier
+	int totalBlocks; 	// blocks in volume
+	int blockSize; 		// size of blocks
+	int freeSpaceLocation; 	// location of the FAT table
+	int rootLocation; 	// location of root directory
+	int firstBlock; 	// location of the first usable block
+	//int totalFreeSpace; 	number of free blocks
+};
+
+// Specifications for directory entry
+struct DE {
+	/* location set to -2 for unused directory entry, 
+	 * location set to positive non-zero value if in use */
+	long location;
+	char name[25];
+	int size;
+
+	// Metadata
+	int dateCreated;
+	int dataModified;
+	int dateLastAccessed;
+
+	char isDirectory; // '0' for directories '1' for non-directories
+};
+/* 
+ * @brief
+ * @param
+ * @param
+ * @return
+ */
+int createDirectory ( int numberOfEntries, struct DE * parent );
+#endif
